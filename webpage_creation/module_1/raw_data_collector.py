92f43cc987c0318279ddca136ff721b4537d2c38
Josjed1 <152343882+Josjed1@users.noreply.github.com>
2024-05-02 21:36:39 -0700

    Added better comments

--- webpage_creation/module_1/raw_data_collector.py
+++ webpage_creation/module_1/raw_data_collector.py
@@ -2,26 +2,30 @@ import requests
 import os
 from bs4 import BeautifulSoup
 
-# This module takes the path argument and creates the raw data from the list of articles provided
-# The input command is "python run.py articles.txt" (This is for the example, default case). It will take the articles.txt and ensures it is a file (even if it is a file path).
-# Once it is verified as a file, it will open the file and read the first URL. Then the web page will be opened and it will gather all of the raw HTML code and store it in
-# article_#_raw.txt, with # being a number. It will then close the article and increase the counter variable, named articleNum, by one, and go until all of the URLs have been
-# read. Then it will return the articleNum variable, which will be used in the processor module.
+
+#This module processes a list of articles specified in a file path provided as an argument, typically in the format "python run.py articles.txt". It verifies the file's existence, then sequentially reads each URL from the file. 
+#For each URL, it fetches the corresponding webpage, extracts the raw HTML code, and saves it into a file named article_#_raw.txt, where # increments for each article. Finally, it returns the total number of articles processed.
 
 def rdc(arguments):
-    articleFile = arguments[1] # Gets the path/file for the articles to be read
-    if(os.path.isfile(articleFile)): # Checks if the path leads to a valid file
-        articleList = open(articleFile, "r") # Opens the file as read only
+    articleFile = arguments[1]  # Gets the path/file for the articles to be read
+    
+    if os.path.isfile(articleFile):  # Checks if the path leads to a valid file
+        articleList = open(articleFile, "r")  
         articleNum = 1
+        
         for URL in articleList.readlines():
-            URL = str(URL[:-1]) # Removes the newline tag from each URL
-            articleName = "article_" + str(articleNum) + "_raw.txt" # Generates the raw file name
-            directory = "Data/raw" # Path for which the file will be made
-            file_path = os.path.join(directory, articleName) # Joins the file name and the file path
-            article = open(file_path, "w", encoding="utf-8") # Opens a writeable file
-            page = requests.get(URL) # Requests the webpage
-            soup = BeautifulSoup(page.content, "html.parser") # Parses the webpage
-            article.write(str(soup)) # Writes the raw HTML into the file
-            articleNum += 1 # Increases the count number
-            article.close() # Closes the file
+            #Generating file names, and the paths that they will take
+            URL = str(URL[:-1])  
+            articleName = "article_" + str(articleNum) + "_raw.txt"  
+            directory = "Data/raw" 
+            file_path = os.path.join(directory, articleName) 
+            
+            # We go to the url and grab the data (HTML) and write with it
+            article = open(file_path, "w", encoding="utf-8") 
+            page = requests.get(URL) 
+            soup = BeautifulSoup(page.content, "html.parser")  
+            article.write(str(soup))  
+            articleNum += 1  
+            article.close()
+            
     return articleNum
